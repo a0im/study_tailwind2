6afe37aba3aace4af697cc85177ea2a6d3cca080
a0im <kzerozero1541@gmail.com>
2022-10-04 02:26:19 +0900

    header1004

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
     extend: {
       width: {
         "1000": "1000px",
+        "180": "180px",
+        "50": "12.5rem",
       },
-    },
-  },
-  plugins: [],
+      height: {
+        "200" : "200px"
+      },
+      lineHeight : {
+        "11": "2.75rem",
+      },
+      fontSize : {
+        "none" : "0"
+      }
+   },
+ns: [],
 };
